a0996a336ab36b73d4164967815fbe91bab6df3d
mike-CVSU <136873036+mike-CVSU@users.noreply.github.com>
2024-11-10 01:25:32 +0800

    Delete fibonacci directory

--- fibonacci/src/fibonacci/fibonacci.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FibonacciController } from './fibonacci.controller';
-
-@Module({
-  controllers: [FibonacciController]
-})
-export class FibonacciModule {}
