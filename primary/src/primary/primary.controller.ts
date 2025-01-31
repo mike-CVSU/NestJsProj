ba4ca51954df45db02b4d97fb3838b0529f98d14
mike-CVSU <136873036+mike-CVSU@users.noreply.github.com>
2025-02-01 03:28:25 +0800

    Update primary.controller.ts

--- primary/src/primary/primary.controller.ts
+++ primary/src/primary/primary.controller.ts
@@ -2,21 +2,22 @@ import {Controller, Get, Param } from '@nestjs/common';
 
 @Controller('primary')
 export class PrimaryController {
-    @Get(':num')
-  identifyNum(@Param('num') num: number) {
-    // Check if the number is less than or equal to 1
-    if (num <= 1) {
-      return { isPrime: false };
-    }
+  @Get(":number")
+  getNumber(@Param('number')number:any){
+   if(number<=1){
 
-    // Check for prime numbers
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) {
-        return { isPrime: false };
+    return{primary:false}
+   }
+   else{
+    for(let i=2;i<=Math.sqrt(number);i++){
+      if(number%i ===0){
+        return{primary:false}
       }
+     
     }
 
-    // If no divisors are found, the number is prime
-    return { isPrime: true };
+   
+   return{primary:true}
   }
+}
 }
